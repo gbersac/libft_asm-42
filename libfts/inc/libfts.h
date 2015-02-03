fd8c0eed9f479e131f871d351d1a637823945608
Guillaume BERSAC <bersac_1@hotmail.fr>
2015-02-03 20:34:39 +0100

    structure

---
+++ libfts/inc/libfts.h
@@ -0,0 +1,2 @@
+void ft_bzero(void *mem, size_t size);
+int my_pow(int, int);
---
+++ libfts/src/bzero.s
@@ -0,0 +1,24 @@
+global _my_pow
+section .text
+
+_my_pow:
+push    rbp             ; create stack frame
+mov     rbp, rsp
+
+cmp     edi, 0          ; Check if base is negative
+mov     eax, 0          ; and return 0 if so
+jl      end
+
+mov     eax, edi        ; grab the "base" argument
+mov     edx, esi        ; grab the "exponent" argument
+
+multiply:
+imul    eax, edi        ; eax * base
+sub     esi, 1          ; exponent - 1
+
+cmp     esi, 1          ; Loop if exponent > 1
+jg      multiply
+
+end:
+pop     rbp             ; restore the base pointer
+ret                     ; return from procedure
