eb098614ac2e3ea6cd96f48af531e318c2b63f18
Guillaume BERSAC <bersac_1@hotmail.fr>
2015-02-06 16:45:31 +0100

    strlen ok

--- libfts/src/ft_strlen.s
+++ libfts/src/ft_strlen.s
@@ -2,3 +2,14 @@ global _ft_strlen
 
 section .text
 _ft_strlen:
+	mov		eax, 0
+
+while:
+	cmp		byte [rdi], 0	;test if we reached '\0'
+	je		return
+	inc		rdi
+	inc		eax
+	jmp		while
+
+return:
+	ret
