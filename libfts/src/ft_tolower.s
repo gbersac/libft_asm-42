46a64c864d5f8f9968c94d15585e9faf8d27a8ce
Guillaume BERSAC <bersac_1@hotmail.fr>
2015-02-06 15:50:33 +0100

    tolower toupper ok

--- libfts/src/ft_tolower.s
+++ libfts/src/ft_tolower.s
@@ -2,3 +2,13 @@ global _ft_tolower
 
 section .text
 _ft_tolower:
+	mov	rax, rdi
+	cmp rdi, 65 
+	jl	ret_
+	cmp rdi, 90
+	jg	ret_
+	add	eax, 32
+	ret
+ 
+ret_:
+	ret
--- libfts/src/ft_toupper.s
+++ libfts/src/ft_toupper.s
@@ -2,3 +2,13 @@ global _ft_toupper
 
 section .text
 _ft_toupper:
+	mov	rax, rdi
+	cmp rdi, 97 
+	jl	ret_
+	cmp rdi, 122
+	jg	ret_
+	sub	eax, 32
+	ret
+ 
+ret_:
+	ret 
