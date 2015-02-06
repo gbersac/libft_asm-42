489a0381c741f63223543cdda49505531c3cbfe5
Guillaume BERSAC <bersac_1@hotmail.fr>
2015-02-06 15:08:28 +0100

    isprint ok

--- libfts/src/ft_isprint.s
+++ libfts/src/ft_isprint.s
@@ -1,4 +1,18 @@
 global _ft_isprint
 
 section .text
-_ft_isprint:
+_ft_isprint
+	cmp		rdi, 32 
+	jb		ret_false
+	cmp		rdi, 126
+	jg		ret_false
+    mov		eax, 1
+	ret
+
+ret_true:
+    mov		eax, 1
+    ret
+
+ret_false:
+    mov		eax, 0
+    ret
