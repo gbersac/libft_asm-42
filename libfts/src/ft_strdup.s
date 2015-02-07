8f2e4e3261ccf3e32429bd84d25d9252242dc9a8
Guillaume BERSAC <bersac_1@hotmail.fr>
2015-02-07 15:01:27 +0100

    strdup

--- libfts/src/ft_strdup.s
+++ libfts/src/ft_strdup.s
@@ -1,4 +1,31 @@
+;char		*ft_strdup(const char *);
+;arg0 = di, arg1 = si, arg3 = dx
+
 global _ft_strdup
 
 section .text
+	extern _ft_strlen
+	extern _malloc
+
 _ft_strdup:
+	push	rbx
+	mov		rbx, rdi
+	call	_ft_strlen	;the result of strlen is in ax register
+	inc		rax
+	mov		rcx, rax
+	mov		rdi, rax
+
+	;call malloc
+	push	rcx
+	call	_malloc
+	pop		rcx
+	test	rax, rax
+	jz		ret_
+	mov		rsi, rbx
+	mov		rdi, rax
+	repnz movsb
+
+ret_:
+	pop		rbx
+	ret
+
