7a53f466f62a3ab5f7ed0c19cfa0cb6727f31f6d
Guillaume BERSAC <bersac_1@hotmail.fr>
2015-02-08 12:20:21 +0100

    strlen ok

--- libfts/src/ft_strlen.s
+++ libfts/src/ft_strlen.s
@@ -1,15 +1,29 @@
+;size_t ft_strlen(const char *);
+; arg0 = di, arg1 = si, arg3 = dx
+
 global _ft_strlen
 
 section .text
 _ft_strlen:
-	mov		rax, 0
+;	mov		rax, 0
+;
+;while:
+;	cmp		byte [rdi], 0	;test if we reached '\0'
+;	je		return
+;	inc		rdi
+;	inc		rax
+;	jmp		while
+;
+;return:
+;	ret
 
-while:
-	cmp		byte [rdi], 0	;test if we reached '\0'
-	je		return
-	inc		rdi
-	inc		rax
-	jmp		while
-
-return:
+	push	rdi
+	sub		ecx, ecx
+	not		ecx
+	sub		al, al
+	cld
+	repne	scasb
+	not		ecx
+	pop		rdi
+	lea		eax, [ecx-1]
 	ret
