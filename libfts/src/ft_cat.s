c0ed4d76bb228d53b9b0363eceebded6789a495c
Guillaume BERSAC <bersac_1@hotmail.fr>
2015-02-08 14:19:53 +0100

    cat ok

--- libfts/src/ft_cat.s
+++ libfts/src/ft_cat.s
@@ -1,31 +1,51 @@
-section .text
-global _ft_cat
-_ft_cat:
-cmp edi, -1
-jle ft_return
-mov rsi, buf
-cat_loop:
-mov rax, 0x2000003 ;read
-mov rdx, bufsize
-syscall
-cmp eax, 0
-jle ft_return
-mov edx, eax
-mov rax, 0x2000004 ;write
-push rdi
-mov rdi, 1
-syscall
-pop rdi
-cmp eax, -1
-je ft_return
-cmp rdi, 0
-je cat_loop
-cmp rax, 8192
-je cat_loop
-ft_return:
-ret
+;void ft_cat(int fd);
+; arg0 = di, arg1 = si, arg3 = dx
+
+;section for uninitialized data
+section .bss
+	buf:
+		resb 255
+
+;section for const
 section .data
-bufsize:
-dd 8192	;8192
-buf:
-times 8192 db 0
+	bufsize:
+		dd 255
+	.len: equ $-bufsize
+
+global _ft_cat
+
+section .text
+_ft_cat:
+	;test invalid fd
+	cmp		edi, -1
+	jle		ft_return
+
+	mov		r8, rdi
+	mov		rsi, buf		;arg1 = buffer
+
+cat_loop:
+	;int read(int fd, void *buffer, int nbyte);
+	mov		rdi, r8				;arg0 = fd
+	mov		rdx, bufsize.len	;arg2 = buffer length
+	mov		rax, 0x2000003		;read
+	syscall
+
+	;test if end of read
+	cmp		rax, 0
+	jle		ft_return
+
+cat_write:
+	;int write(int fd, const void *buf, size_t nbyte);
+	mov		rdi, 1			;arg0 = fd
+	mov		rdx, rax		;arg2 = buffer length
+	mov		rax, 0x2000004	;write
+	syscall
+
+	;test for failure of write
+	cmp		rax, -1
+	jle		ft_return
+
+	jmp		cat_loop
+
+ft_return:
+	ret
